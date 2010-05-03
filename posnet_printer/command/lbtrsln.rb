e141d96cb64456c0b32ad349462a882637401bc2
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-05-03 15:36:46 +0200

    PTU support for LBTRSLN

--- posnet_printer/command/lbtrsln.rb
+++ posnet_printer/command/lbtrsln.rb
@@ -2,11 +2,12 @@ class Posnet::Command::LBTRSLN < Posnet::Command
   escp :checksum => true
 
 
-  def process_command(line_number, name, quantity, price, gross_price)
+  def process_command(line_number, name, quantity, price, ptu = " ")
     @quantity = quantity
     @price = price.to_s_price
-    @gross_price = gross_price.to_s_price
-    "#{line_number}$l#{name[0,40].to_mazovia}\r#{formatted_quantity}\r /#{@price}/#{@gross_price}/"
+    @gross_price = (quantity*price).to_s_price
+    @ptu = ptu.to_s.upcase[0,1]
+    "#{line_number}$l#{name[0,40].to_mazovia}\r#{formatted_quantity}\r#{@ptu}/#{@price}/#{@gross_price}/"
   end
 
   def formatted_quantity
